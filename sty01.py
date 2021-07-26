777a62e87db048428f01be5de258027bc600709f
Choi_AY <cayoung0205@gmail.com>
2021-07-26 18:55:38 +0900

    for_sty

---
+++ sty01.py
@@ -0,0 +1,6 @@
+## 입력한 수를 그 수 만큼 반복하여 출력(for)
+
+num = int(input(":"))
+
+for i in range(num):
+    print(num)
---
+++ sty02.py
@@ -0,0 +1,6 @@
+## 정수를 입력 받dk 입력 받은 수의 각각을 제곱하여 출력(for)
+
+num = int(input(':'))
+
+for i in range(1, num+1):
+    print(i, i*i)
